cf4b67741a6a66d40a659085eb8e662b7494da9e
jaq23369 <jaq23369@uvg.edu.gt>
2023-07-22 14:11:10 -0600

    Quinta Parte, asignacion de funciones a Enemigo1 y Enemigo2
    Aqui solamente se le aplicaron las funciones a los enemigos establecidos en el juego, en el caso del enemigo 2 se le asignaron y agregaron unas para asi obtener un movimiento dinamico

--- Codigo Greenfoot.java
+++ Codigo Greenfoot.java
@@ -359,3 +359,62 @@ public class Enemigos extends Movers
     }
 
 }
+
+"Quinta parte, asignacions de funciones para Enemigo1 y Enemigo2"
+public class Enemigo1 extends Enemigos
+{
+    /**
+     * Act - do whatever the Enemigo1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Enemigo1()
+    {
+         getImage().scale (getImage().getWidth()/2, getImage().getHeight()/2);
+    }
+    public void act()
+    {
+        move(5);
+        wrapAtEdge();
+    }
+}
+
+public class Enemigo2 extends Enemigos
+{
+    /**
+     * Act - do whatever the Enemigo2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    boolean upDown;
+    int velocidad = 2;
+    public Enemigo2(boolean upAndDown)
+    {
+        getImage().scale (getImage().getWidth()/2, getImage().getHeight()/2);
+        upDown =  upAndDown;
+    }
+    public void act()
+    {
+        moveAround();
+        enemyHitWalls();
+        
+    }
+    public void enemyHitWalls()
+    {
+        if(hitwalls() || isAtEdge())
+        {
+            velocidad = -velocidad;
+        } 
+    }
+    private void moveAround()
+    {
+        int x = getX();
+        int y = getY();
+        if(upDown)
+        {
+          setLocation(x,y + velocidad);  
+        }
+        else if (!upDown)
+        {
+            setLocation(x + velocidad, y);
+        }
+    }
+}
